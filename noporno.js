3a4f50795b98dfd478906371fea00fd26ba10fc9
ut1s <110339660+ut1s@users.noreply.github.com>
2023-03-22 16:48:17 +0100

    Added site popping up on a bad site

--- noporno.js
+++ noporno.js
@@ -4,13 +4,14 @@ function find() {
   let href = window.location.href;
   for(let i = 0; i <= badsites.length; i++){
     if (href.includes(badsites[i])) {
-      window.stop();
-      window.location.replace(goodsites[Math.floor(Math.random()*goodsites.length)]);
+      flush();
+      //window.location.replace(goodsites[Math.floor(Math.random()*goodsites.length)]);
     }
   }
 }
 
 function flush() {
-  document.body.innerHTML = "";
-  
+  document.open();
+  document.write(bsite);
+  document.close();
 }
