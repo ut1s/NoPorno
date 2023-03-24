a07b8a22f7dbd105c25bf48aacd667c5e7e085f0
ut1s <110339660+ut1s@users.noreply.github.com>
2023-03-24 07:32:18 +0100

    Updated lists

--- goodsites.js
+++ goodsites.js
@@ -1,1 +1,1 @@
-var goodsites = ["https://music.youtube.com", "https://open.spotify.com/", "https://toliver.hu"];
+var goodsites = ["https://music.youtube.com", "https://open.spotify.com/", "https://toliver.hu", "https://www.urbandictionary.com/define.php?term=Porn", "https://www.urbandictionary.com/define.php?term=Pornhub"];
