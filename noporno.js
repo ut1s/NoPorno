703a7d9f08236792e8e86c5abb76d5392af2bc83
ut1s <110339660+ut1s@users.noreply.github.com>
2023-03-27 18:54:27 +0200

    Added blocking message for reddits

--- noporno.js
+++ noporno.js
@@ -9,7 +9,7 @@ function find() {
       for(let i = 0; i <= subreddits.length; i++){
         if (href.includes(subreddits[i])) {
           console.log("flush!");
-          flush(bsite);
+          flush(rsite);
           console.log("flushed");
           wait(1);
           console.log("timed");
