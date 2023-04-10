d0faf22fc8421ad845028245a3d4bea6c0205948
ut1s <110339660+ut1s@users.noreply.github.com>
2023-04-10 19:35:58 +0200

    Typo

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 var goodsites = [
   "https://music.youtube.com",
   "https://open.spotify.com/",
-  "https://toliver.hu",
   "https://www.urbandictionary.com/define.php?term=Porn",
   "https://www.urbandictionary.com/define.php?term=Pornhub",
 ];
